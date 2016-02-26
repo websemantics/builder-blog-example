becac73cc80b9dc6349c4d0c5ecc11fa98e5701a
Adnan <adnan@websemantics.ca>
2016-02-25 22:21:36 -0500

    Initial commit

---
+++ resources/core/config/streams/addons.php
@@ -0,0 +1,31 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Eager Addons
+    |--------------------------------------------------------------------------
+    |
+    | Eager loaded addons are registered first and can be defined
+    | here by specifying their relative path to the addon's root.
+    |
+    */
+
+    'eager' => [
+        'core/anomaly/redirects-module'
+    ],
+
+    /*
+    |--------------------------------------------------------------------------
+    | Deferred Addons
+    |--------------------------------------------------------------------------
+    |
+    | Deferred addons are registered last and can be defined
+    | here by specifying their relative path to the addon's root.
+    |
+    */
+
+    'deferred' => []
+
+];
